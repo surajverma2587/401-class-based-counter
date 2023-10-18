8fb039af59eca63983128c8ae52bcea5f20c7667
surajverma2587 <surajverma2587@gmail.com>
2023-10-18 19:46:10 +0100

    styled components and state

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,11 @@
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="card m-2 p-4">
+      <h1 className="text-center">React Counter</h1>
+      <hr />
+      <p className="text-center text-muted">
+        Please use the buttons to interact with your counter
+      </p>
+    </div>
+  );
 };
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,56 @@
-export const Counter = () => {
-  return <div>Counter</div>;
-};
+import { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    // declare state
+    this.state = { counter: 0 };
+  }
+
+  handleIncrement = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({
+      counter: this.state.counter > 1 ? this.state.counter - 1 : 0,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({ counter: 0 });
+  };
+
+  render() {
+    return (
+      <div className="card m-2 p-4">
+        <h3 className="text-center">Counter: {this.state.counter}</h3>
+
+        <div className="btn-group" role="group">
+          <button
+            onClick={this.handleIncrement}
+            type="button"
+            className="btn btn-success"
+          >
+            Increment
+          </button>
+          <button
+            onClick={this.handleReset}
+            type="button"
+            className="btn btn-danger"
+          >
+            Reset
+          </button>
+          <button
+            onClick={this.handleDecrement}
+            type="button"
+            className="btn btn-warning"
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
